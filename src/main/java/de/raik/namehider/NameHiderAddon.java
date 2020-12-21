e299a445e0fcb84c1800f151019fe4272698e0ec
MineFlash07 <mineflash07@gmail.com>
2020-12-21 11:31:13 +0100

    created addon class

---
+++ src/main/java/de/raik/namehider/NameHiderAddon.java
@@ -0,0 +1,31 @@
+package de.raik.namehider;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+/**
+ * Addon instance
+ * to manage the addon
+ *
+ * @author Raik
+ * @version 1.0
+ */
+public class NameHiderAddon extends LabyModAddon {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+}
