4ddf6b1861b83dfa326a4571f3beed2291c37215
MineFlash07 <mineflash07@gmail.com>
2020-12-21 12:12:36 +0100

    added overridding for renderName method

--- src/main/java/de/raik/namehider/implementation/HiderRenderPlayerImplementation.java
+++ src/main/java/de/raik/namehider/implementation/HiderRenderPlayerImplementation.java
@@ -1,6 +1,8 @@
 package de.raik.namehider.implementation;
 
 import net.labymod.core_implementation.mc18.RenderPlayerImplementation;
+import net.labymod.mojang.RenderPlayerHook;
+import net.minecraft.client.entity.AbstractClientPlayer;
 
 /**
  * New render implementation to access
@@ -10,4 +12,20 @@ import net.labymod.core_implementation.mc18.RenderPlayerImplementation;
  * @version 1.0
  */
 public class HiderRenderPlayerImplementation extends RenderPlayerImplementation {
+
+    /**
+     * Overriding the renderName method to
+     * apply wanted changes
+     *
+     * @param renderPlayer renderPlayer
+     * @param entity the entity to render
+     * @param x x
+     * @param y y
+     * @param z z
+     */
+    @Override
+    public void renderName(RenderPlayerHook.RenderPlayerCustom renderPlayer, AbstractClientPlayer entity, double x, double y, double z) {
+
+    }
+
 }
