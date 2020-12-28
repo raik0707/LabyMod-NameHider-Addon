79e48c09b9608bd28a1032d6aee8e962b476a5da
MineFlash07 <mineflash07@gmail.com>
2020-12-28 19:59:47 +0100

    added change tag entry

---
+++ src/main/java/de/raik/namehider/playermenu/entries/ChangeTagEntry.java
@@ -0,0 +1,77 @@
+package de.raik.namehider.playermenu.entries;
+
+import de.raik.namehider.playermenu.AddonEntryExecutor;
+import de.raik.namehider.playermenu.AddonMenuEntry;
+import de.raik.namehider.playermenu.PlayerMenuEditor;
+import net.labymod.gui.GuiTagsAdd;
+import net.labymod.settings.elements.ControlElement;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class ChangeTagEntry extends AddonMenuEntry {
+
+    /**
+     * Constructor to set player menu entry
+     *
+     * @param playerMenuEditor playerMenuEditor
+     */
+    public ChangeTagEntry(PlayerMenuEditor playerMenuEditor) {
+        super(playerMenuEditor);
+
+        //Set executor to call execute
+        this.setExecutor(new AddonEntryExecutor(this::execute));
+    }
+
+    /**
+     * Method called on execute by the executor
+     * calling gui yes no to handle removing
+     *
+     * @param entityPlayer The entityPlayer
+     */
+    private void execute(EntityPlayer entityPlayer) {
+        Minecraft.getMinecraft().displayGuiScreen(new GuiTagsAdd(null, entityPlayer.getName()));
+    }
+
+    /**
+     * Method to set the name of a command
+     * in a sub class
+     *
+     * @return the name
+     */
+    @Override
+    public String getName() {
+        return "Change tag";
+    }
+
+    /**
+     * Method to set the name of the config
+     *
+     * @return the setting name
+     */
+    @Override
+    public String getSettingName() {
+        return "changetag";
+    }
+
+    /**
+     * Method to set the icon of a command
+     * in a sub class
+     *
+     * @return the icon
+     */
+    @Override
+    public ControlElement.IconData getIcon() {
+        return new ControlElement.IconData("labymod/textures/chat/gui_editor.png");
+    }
+
+    /**
+     * Method to set the description of a command
+     * in a sub class
+     *
+     * @return the description
+     */
+    @Override
+    public String getDescription() {
+        return "Entry to change the tag if player. Will add if not present.";
+    }
+}
